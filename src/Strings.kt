4c863da49f117353991785d986c18026c8220452
jotabass <jotabelk@gmail.com>
2022-01-01 09:43:22 -0300

    Strings

--- src/Strings.kt
+++ src/Strings.kt
@@ -3,7 +3,7 @@
  */
 fun main() {
     val clientes = listOf("joao","pedro","sara","paulo") // criado uma lista de clientes
-    println("O primeiro cliente atendido foi: ${clientes[0]}")
+    println("O primeiro cliente atendido foi: ${clientes[0]}") // chamando os clientes na ordem definida
     println("O primeiro cliente a chegar na clinica foi: ${clientes[2]}")
     println("O ultimo cliente da lista é o: ${clientes[3]}")
 }
