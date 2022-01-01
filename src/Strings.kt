097e9f27209b42def711ed7d2a87a53665569009
jotabass <jotabelk@gmail.com>
2022-01-01 10:18:14 -0300

    Strings com mais exemplos

--- src/Strings.kt
+++ src/Strings.kt
@@ -6,4 +6,17 @@ fun main() {
     println("O primeiro cliente atendido foi: ${clientes[0]}") // chamando os clientes na ordem definida
     println("O primeiro cliente a chegar na clinica foi: ${clientes[2]}")
     println("O ultimo cliente da lista é o: ${clientes[3]}")
+
+    println("------------------outro exemplo no uso de Strings---------------------------")
+
+    val humor = false // boleano que indica verdadeiro
+    if (humor == true){
+        println("hoje voce esta feliz")  }else{
+        println("Hoje voce esta chateado") }
+
+
+    println("------------ ---reduzindo o numero de linha no código-----------------------")
+    // código mais enxuto
+    val humor2 = true
+    println("Hoje voce esta ${if (humor2)"feliz" else "chateado"}")
 }
