7e7b9a615854e8019647834e4c5546184f7ed137
jotabass <jotabelk@gmail.com>
2022-01-06 12:55:17 -0300

    primeira alteração

--- src/Classes.kt
+++ src/Classes.kt
@@ -1,10 +1,20 @@
+/**
+ * neste projeto a intenção é fazer cadastro de membros de uma igreja com dados simples do tipo:
+ * nome, sexo, cpf, rg, idade etc...
+ */
+
 class Igreja{
-    var membros = mutableListOf<String>() //lista mutavel criada para adcionar membros
+    var nome = mutableListOf<String>() //lista mutavel criada para adcionar membros
+    var idade = mutableListOf<Int>()
 }
 
 fun main() {
     val cadastro = Igreja()
-    cadastro.membros.add(readLine().toString()) // agora falta acrecestar mais de um dado na lista
-    //cadastro.membros.add(readLine().toString())
-    println(cadastro.membros)
+    print("Digite seu nome ")
+    cadastro.nome.add(readLine().toString()) // agora falta acrecestar mais de um dado na lista
+    print("Digite sua idade ")
+    cadastro.idade.add(readLine()!!.toInt())
+    println("Exibindo lita de nomes -> ${cadastro.nome}")
+    println(cadastro.nome + cadastro.idade)
+   // println("${cadastro.nome} tem ${cadastro.idade} anos de idade.")
 }
